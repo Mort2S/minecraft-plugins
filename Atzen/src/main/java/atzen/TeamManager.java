9a51d6a5c6d290ba3ac4c02c3e087713c5526866
Moritz Pollert <pollertmoritz@gmail.com>
2025-04-21 13:47:13 +0200

    Added an event listener to save backpacks on inventory close

--- Atzen/src/main/java/atzen/TeamManager.java
+++ Atzen/src/main/java/atzen/TeamManager.java
@@ -5,14 +5,19 @@ import java.util.List;
 import java.util.Map;
 
 import org.bukkit.Bukkit;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.entity.HumanEntity;
 import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.inventory.InventoryCloseEvent;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.scoreboard.Scoreboard;
 import org.bukkit.scoreboard.Team;
 
-public class TeamManager {
+public class TeamManager implements Listener {
 
     private final JavaPlugin plugin;
     private final Map<String, Inventory> teamBackpacks = new HashMap<>();
@@ -21,6 +26,8 @@ public class TeamManager {
     public TeamManager(JavaPlugin plugin) {
         this.plugin = plugin;
         this.playerTeams = new HashMap<>();
+
+        Bukkit.getPluginManager().registerEvents(this, plugin);
     }
 
     public Team getPlayerTeam(Player player) {
@@ -47,26 +54,43 @@ public class TeamManager {
     }
 
     public void saveBackpacks() {
+        FileConfiguration config = plugin.getConfig();
+
         for (Map.Entry<String, Inventory> entry : teamBackpacks.entrySet()) {
             String teamName = entry.getKey();
             Inventory inv = entry.getValue();
-
-            plugin.getConfig().set("backpacks." + teamName, inv.getContents());
+            config.set("backpacks." + teamName, inv.getContents());
         }
+
         plugin.saveConfig();
     }
 
     public void loadBackpacks() {
-        if (!plugin.getConfig().isConfigurationSection("backpacks")) {
-            return;
-        }
+        if (!plugin.getConfig().isConfigurationSection("backpacks")) return;
 
         for (String teamName : plugin.getConfig().getConfigurationSection("backpacks").getKeys(false)) {
-            ItemStack[] contents = ((List<ItemStack>) plugin.getConfig().get("backpacks." + teamName)).toArray(new ItemStack[0]);
+            List<ItemStack> itemList = (List<ItemStack>) plugin.getConfig().get("backpacks." + teamName);
+            ItemStack[] contents = itemList.toArray(new ItemStack[0]);
+
             Inventory inv = Bukkit.createInventory(null, 36, "Team Backpack: " + teamName);
             inv.setContents(contents);
             teamBackpacks.put(teamName, inv);
         }
     }
 
+    @EventHandler
+    public void onBackpackClose(InventoryCloseEvent event) {
+        HumanEntity human = event.getPlayer();
+        if (!(human instanceof Player player)) return;
+
+        Inventory closedInv = event.getInventory();
+
+        for (Map.Entry<String, Inventory> entry : teamBackpacks.entrySet()) {
+            if (entry.getValue().equals(closedInv)) {
+                plugin.getConfig().set("backpacks." + entry.getKey(), closedInv.getContents());
+                plugin.saveConfig();
+                return;
+            }
+        }
+    }
 }
